0720c0a3fe36fe4b42f1e69c110b86c5a29e1573
FredericoVillani <34401583+FredericoVillani@users.noreply.github.com>
2018-07-05 00:15:12 -0300

    Create niobio.php

---
+++ admin/language/english/payment/niobio.php
@@ -0,0 +1,36 @@
+<?php
+
+//Payment page links
+$_['text_niobio'] = '<a onclick="window.open(\'http://www.niobiocash.org/en/\');"><img src="view/image/payment/karbo.png" alt="Niobio Cash" title="Niobio Cash" style="border: 1px solid #EEEEEE;" /></a>';
+
+// Heading
+$_['heading_title']			= 'Niobio Cash';
+
+// Settings
+$_['wallet_address']			= 'Wallet store';
+$_['wallet_type']			= 'Servise type';
+$_['wallet_type_simplewallet']		= 'SimpleWallet';
+$_['wallet_type_walletd']		= 'WalletD';
+$_['wallet_type_gateway']		= 'Payment gateway';
+$_['wallet_host']			= 'Host';
+$_['wallet_port']			= 'Port';
+$_['wallet_ssl']			= 'SSL';
+$_['wallet_tx_conf']			= 'Number of confirmations';
+$_['niobio_yes']				= 'Yes';
+$_['niobio_no']				= 'No';
+$_['niobio_enable']			= 'Enable';
+$_['niobio_disable']			= 'Disable';
+$_['niobio_order_status']		= 'Initial order status';
+$_['niobio_order_payment_status']	= 'Order status after payment';
+$_['niobio_geo_zone']			= 'Geographical zone';
+$_['niobio_status']			= 'Status';
+$_['niobio_sort_order']			= 'Sort Order';
+
+// Text
+$_['text_payment']			= 'Payment';
+$_['text_success']			= 'Success: You have modified Niobio Cash payment module!';
+
+// Error
+$_['error_permission']			= 'Warning: You do not have permission to modify payment Niobio Cash!';
+
+?>
