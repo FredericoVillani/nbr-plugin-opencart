dccc6c21697633df54da641207cfdeab38123d8b
FredericoVillani <34401583+FredericoVillani@users.noreply.github.com>
2018-07-05 00:16:06 -0300

    Create niobio.php

---
+++ admin/language/russian/payment/niobio.php
@@ -0,0 +1,36 @@
+<?php
+
+//Payment page links
+$_['text_niobio'] = '<a onclick="window.open(\'http://www.niobiocash.org\');"><img src="view/image/payment/karbo.png" alt="Украинская криптовалюта «Карбо»" title="Украинская криптовалюта «Карбо»" style="border: 1px solid #EEEEEE;" /></a>';
+
+// Heading
+$_['heading_title']			= 'Niobio Cash';
+
+// Settings
+$_['wallet_address']			= 'Кошелек магазина';
+$_['wallet_type']			= 'Тип сервиса';
+$_['wallet_type_simplewallet']		= 'SimpleWallet';
+$_['wallet_type_walletd']		= 'WalletD';
+$_['wallet_type_gateway']		= 'Платежный шлюз';
+$_['wallet_host']			= 'Хост';
+$_['wallet_port']			= 'Порт';
+$_['wallet_ssl']			= 'SSL';
+$_['wallet_tx_conf']			= 'Количество подтверждений';
+$_['niobio_yes']				= 'Да';
+$_['niobio_no']				= 'Нет';
+$_['niobio_enable']			= 'Включено';
+$_['niobio_disable']			= 'Отключено';
+$_['niobio_order_status']		= 'Начальный статус заказа';
+$_['niobio_order_payment_status']	= 'Статус заказа после оплаты';
+$_['niobio_geo_zone']			= 'Географическая зона';
+$_['niobio_status']			= 'Состояние';
+$_['niobio_sort_order']			= 'Порядок сортировки';
+
+// Text
+$_['text_payment']			= 'Оплата';
+$_['text_success']			= 'Настройки успешно сохранены!';
+
+// Error
+$_['error_permission']			= 'Вы не имеете достаточно привилегий для сохранения настроек!';
+
+?>
