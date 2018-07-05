0720e0207cea43c314f692607e997a46cad06510
FredericoVillani <34401583+FredericoVillani@users.noreply.github.com>
2018-07-05 00:00:42 -0300

    Create niobio.php

---
+++ catalog/language/english/payment/niobio.php
@@ -0,0 +1,8 @@
+<?php
+
+$_['text_title']	= 'Payment by crypto currency «Niobio Cash»';
+$_['text_information']	= 'Payment instruction';
+$_['text_description']	= 'Your order is generated for sending. After clicking on the "Order Confirmation" button, the system will create a new order with the status of waiting for payment, and you are moved to the payment order page. As soon as you pay it and the coins go to the store account, the order will automatically change its status and will be sent to the processing for sending. <br /> <br />Niobio Cash - it is an egalitarian decentralized system where the value of a coin is based solely on demand and supply on the market, where the emission depends on the constant mathematical algorithm of the system, where everyone can take part in the emission of new coins, receiving them as a reward for the resources provided by their equipment.';
+$_['text_payment']	= 'Received payment for your order №';
+
+?>
